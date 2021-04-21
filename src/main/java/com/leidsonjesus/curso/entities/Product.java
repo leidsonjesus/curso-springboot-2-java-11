0721c9fe9052c01cb522c7035de8e12360f25d25
Leidson Jesus <leidson18e@gmail.com>
2021-04-21 20:31:19 -0300

    Exception handling - update

--- src/main/java/com/leidsonjesus/curso/entities/Product.java
+++ src/main/java/com/leidsonjesus/curso/entities/Product.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
-@Table(name = " tb_product")
+@Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
